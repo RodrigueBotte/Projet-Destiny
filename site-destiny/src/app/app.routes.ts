e8ead5878e510e6c24fc5b27b4c8199c7d9a05c7
unknown <rodrigue.botte@gmail.com>
2024-06-24 21:00:49 +0200

    avancement de la page principale

--- site-destiny/src/app/app.routes.ts
+++ site-destiny/src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 import { AccueilComponent } from './accueil/accueil.component';
 
 export const routes: Routes = [
-    {path: "", component: AccueilComponent},
+    // {path: "", component: AccueilComponent},
 ];
