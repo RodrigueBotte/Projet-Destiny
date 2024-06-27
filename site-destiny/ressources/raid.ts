ea2fbeda96ed0c959923f89fd2f23588d20d90ae
rodrigue botte <rodrigue.botte@gmail.com>
2024-06-27 16:27:33 +0200

    ajout fichier json

---
+++ site-destiny/ressources/raid.ts
@@ -0,0 +1,4 @@
+export type raid={
+    titre: string,
+    img: string
+}
---
+++ site-destiny/ressources/raidList.ts
@@ -0,0 +1,41 @@
+import { raid } from "./raid";
+
+
+export const RAID: raid[] = [
+    {
+        titre :"La Chute du Roi",
+        img: "chuteRoi.jpg"
+    },
+    {
+        titre :"Le Caveau de Verre",
+        img: "caveauVerre.jpg"
+    },
+    {
+        titre :"La Chute de Cropta",
+        img: "chuteCropta.jpg"
+    },
+    {
+        titre :"Le Jardin du Salut",
+        img: "jardinSalut.jpg"
+    },
+    {
+        titre :"La Crypte de la Pierre",
+        img: "cryptePierre.jpg"
+    },
+    {
+        titre :"Le dernier Voeux",
+        img: "dernierVoeux.jpg"
+    },
+    {
+        titre :"Origine du Cauchemar",
+        img: "origineCauchemar.jpg"
+    },
+    {
+        titre :"L'Orée du Salut",
+        img: "salvationsEdge.jpg"
+    },
+    {
+        titre :"Le Serment du Disciple",
+        img: "sermentDisciple.jpg"
+    },
+]
