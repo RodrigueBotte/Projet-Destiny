8af777a1681ebd9389645dcd7e1394d7e4055485
unknown <rodrigue.botte@gmail.com>
2024-06-28 12:22:33 +0200

    finalisation de la page actu et début de la page raid

--- site-destiny/src/app/raid-planning/raid-planning.component.ts
+++ site-destiny/src/app/raid-planning/raid-planning.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-raid-planning',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './raid-planning.component.css'
 })
 export class RaidPlanningComponent {
-
+  constructor(private router: Router){}
+  
+  goToActu(){
+    // test pour voir si le click se déclenche bien 
+    console.log('bonjour');
+    // Chemin permettant d'aller à la section actu 
+    this.router.navigate(['./actuPath'])
+  }
+  goToBuild(){
+    this.router.navigate(['./buildPath'])
+  }
+  goToRaid(){
+    this.router.navigate(['./raidPath'])
+  }
+  goToGuide(){
+    this.router.navigate(['./guidePath'])
+  }
+  goToAccueil(){
+    this.router.navigate(['./accueilPath'])
+  }
 }
